b22e478402ed2b5ac8a7c9fc48ba95307fbf9382
dqLaptop <isabelmm331@gmail.com>
2022-11-02 21:50:46 +0100

    Añadiendo estilo menu pestañas y tema claro

--- javascript-node-webpack-main/src/index.js
+++ javascript-node-webpack-main/src/index.js
@@ -1,2 +1,6 @@
+import './styles.css';
+import { Panel } from './Classes/index';
 
-
+export const PanelPendiente=new Panel(1);
+export const PanelProgreso=new Panel(2);
+export const PanelHecho=new Panel(3);
