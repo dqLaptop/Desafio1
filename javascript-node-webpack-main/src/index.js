83b9d725561f7d5225ced4f58e0afa91231b64dd
dqLaptop <isabelmm331@gmail.com>
2022-11-02 18:10:44 +0100

    Añadiendo estructura pagina, estilo oscuro con menu botones

--- javascript-node-webpack-main/src/index.js
+++ javascript-node-webpack-main/src/index.js
@@ -1,10 +1,2 @@
 
-import './css/normalize.css';
-import './css/skeleton.css';
-import './styles.css';
-import { Tarea, ListaTarea } from './classes/index.js';
-
-export const ListaTarea = new ListaTarea();
-export const ListaProgreso = new ListaTarea();
-export const ListaHecho = new ListaTarea();
 
